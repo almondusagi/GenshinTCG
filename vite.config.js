dd5cfbd0dc4a9dec293545d791726dba587e027c
almondusagi <attoterui@gmail.com>
2026-04-18 10:21:18 +0900

    fix actions

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   // GitHub Pages は https://ユーザー名.github.io/リポジトリ名/ で配信されるため、
   // リポジトリ名をそのまま base に設定する必要があります。
-  // リポジトリ名が "GITCG-deck" 以外の場合はここを変更してください。
+  // リポジトリ名が "GenshinTCG" 以外の場合はここを変更してください。
   base: '/GITCG-deck/',
 })
